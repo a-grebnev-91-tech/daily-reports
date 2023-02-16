59ff066afe40acf551410142ab1815165fe0a50e
a-grebnev-91-tech <a.grebnev.91.tech@yandex.ru>
2023-02-16 17:43:49 +0300

    init commit

---
+++ src/main/java/a/grebnev/dailyreports/DailyReportsApplication.java
@@ -0,0 +1,13 @@
+package a.grebnev.dailyreports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyReportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyReportsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/a/grebnev/dailyreports/DailyReportsApplicationTests.java
@@ -0,0 +1,13 @@
+package a.grebnev.dailyreports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyReportsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
